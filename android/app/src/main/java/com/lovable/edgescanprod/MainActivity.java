30c7f25974b6b1c0758fb7f8dcd991138907453e
lapalma2025 <officejusttalk@gmail.com>
2025-10-26 15:02:45 +0100

    sync

---
+++ android/app/src/main/java/com/lovable/edgescanprod/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lovable.edgescanprod;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,21 @@
-import { CapacitorConfig } from '@capacitor/core';
+import { CapacitorConfig } from "@capacitor/core";
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.2af7fa1ee5a84405ad7dd954de78851d',
-  appName: 'edge-scan-pro',
-  webDir: 'dist',
-  server: {
-    url: 'https://2af7fa1e-e5a8-4405-ad7d-d954de78851d.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    Camera: {
-      permissions: ['camera', 'photos']
-    },
-    Filesystem: {
-      permissions: ['camera', 'photos']
-    }
-  }
+	appId: "com.lovable.edgescanprod",
+	appName: "edge-scan-pro",
+	webDir: "dist",
+	server: {
+		url: "http://192.168.0.193:5173",
+		cleartext: true,
+	},
+	plugins: {
+		Camera: {
+			permissions: ["camera", "photos"],
+		},
+		Filesystem: {
+			permissions: ["camera", "photos"],
+		},
+	},
 };
 
 export default config;
